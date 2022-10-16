08ab740bb5f3b7dbb6074e5b7ff4171ff2296b4c
Elwardi <elwardi.fadeli@tu-darmstadt.de>
2022-10-16 17:26:44 +0200

    add Ex 05 solution

--- exercises/parallelClass.C
+++ exercises/parallelClass.C
@@ -12,7 +12,11 @@ bool Foam::parallelClass::checkPosition(const vector& position) const
     // This function must return true if the position is inside the global mesh
     // and false otherwise
 
-    return false;
+    return returnReduce
+    (
+        mesh_.findCell(position) != -1,
+        orOp<bool>()
+    );
 }
 
 Foam::label Foam::parallelClass::whoHasReferenceCell(const vector& position) const
@@ -20,7 +24,12 @@ Foam::label Foam::parallelClass::whoHasReferenceCell(const vector& position) con
     // This function must return the process ID where the cell associated with
     // position is at; and -1 if position is outside the global mesh
 
-    return -1;
+    boolList ids(Pstream::nProcs(), false);
+    ids[Pstream::myProcNo()] = mesh_.findCell(position) != -1;
+    Pstream::gatherList(ids);
+    Pstream::scatterList(ids);
+
+    return findIndex(ids, true);
 }
 
 }
